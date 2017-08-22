77d69767138189e3884865fe5f4f68019bfefe72
rroppolo <rachel.roppolo@act.md>
2017-08-22 15:51:56 -0400

    Add missing files

---
+++ lib/src/components/search/filters/facet-filter/MenuRefinementListFilter.d.ts
@@ -0,0 +1,5 @@
+import { MenuFilter } from "./MenuFilter";
+import { MultiFieldFacetAccessor } from '../../../../core';
+export declare class MenuRefinementListFilter extends MenuFilter {
+    defineAccessor(): MultiFieldFacetAccessor;
+}
